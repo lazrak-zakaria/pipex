1bd432238cd74c909a4439734811d8bc53594153
zakaria lazrak <z.zakarialazrak@gmail.com>
2023-01-03 12:59:27 +0100

    pipex +function makefile relink

---
+++ pipex0.7/bonus/libft2.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zlazrak <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/31 11:49:05 by zlazrak           #+#    #+#             */
+/*   Updated: 2022/12/31 11:49:07 by zlazrak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex_bonus.h"
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*answer;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	answer = malloc(sizeof(char) * (i + j + 1));
+	if (!answer)
+		return (NULL);
+	i = -1;
+	j = 0;
+	while (s1[++i])
+		answer[i] = s1[i];
+	while (s2[j])
+		answer[i++] = s2[j++];
+	answer[i] = '\0';
+	return (answer);
+}
+
+int	ft_cmp(char *a, char *b, int n)
+{
+	int	i;
+
+	i = 0;
+	while (a[i] && b[i] && a[i] == b[i])
+		i++;
+	if (i == n)
+		return (1);
+	return (0);
+}
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	ft_find(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (1);
+		s++;
+	}
+	return (0);
+}
---
+++ pipex0.7/bonus/pipex_bonus.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zlazrak <zlazrak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/31 11:49:16 by zlazrak           #+#    #+#             */
+/*   Updated: 2023/01/01 14:09:01 by zlazrak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex_bonus.h"
+
+void	ft_first(char **av, char **env, int *pfd)
+{
+	int		fd;
+	int		i;
+
+	fd = here_doc(av, &i);
+	if (fd < 0)
+	{
+		perror("Error");
+		exit(1);
+	}
+	close(pfd[0]);
+	check_dup2(fd, 0, pfd, 1);
+	close(fd);
+		ft_exec(av, env, i);
+}
+
+void	ft_second(int ac, char **av, char **env, int *pfd)
+{
+	t_arg	arg;
+	pid_t	pid;
+	int		i;
+
+	i = 2;
+	arg.env = env;
+	arg.av = av;
+	if (ft_cmp(av[1], "here_doc", 8))
+		i = 3;
+	while (++i < ac - 1)
+	{
+		pipe(pfd);
+		pid = fork();
+		if (!pid)
+		{
+			ft_third(ac, &arg, pfd, i);
+		}
+		else
+		{
+			wait(0);
+			dup2(pfd[0], 0);
+			close(pfd[0]);
+			close(pfd[1]); //why because ine thne next fork i will read from the pipe  
+		}
+	}
+}
+
+void	ft_third(int ac, t_arg *arg, int*pfd, int i)
+{
+	ft_final_cmd(ac, arg->av, pfd, i);
+	if (dup2(pfd[1], 1) < 0)
+		ft_print_error(NULL, NULL, "Error dup\n");
+	close(pfd[0]);
+	close(pfd[1]);
+
+	ft_exec(arg->av, arg->env, i);
+}
+
+void	ft_pipex_bonus(int ac, char **av, char **env)
+{
+	int		pipe_fd[2];
+	pid_t	pid;
+
+	pipe(pipe_fd);
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("Error");
+		exit(EXIT_FAILURE);
+	}
+	else if (!pid)
+		ft_first(av, env, pipe_fd);
+	else
+	{
+		wait(NULL);
+		if (ft_cmp(av[1], "here_doc", 8))
+			unlink("here_doc");
+		dup2(pipe_fd[0], 0);
+		close(pipe_fd[1]);
+		close(pipe_fd[0]);
+		ft_second(ac, av, env, pipe_fd);
+	}
+}
+
+int	main(int ac, char **av, char **env)
+{
+	if (ac < 5)
+		return (0);
+	ft_pipex_bonus(ac, av, env);
+	return (0);
+}
